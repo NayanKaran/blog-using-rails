58f1ba3cdd0814d25cb5d17a49c36298a3c73de5
Debabrata Mandal <bapidebabrata@gmail.com>
2022-09-17 21:42:55 +0530

    Fix typos

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,6 +5,6 @@ class Comment < ApplicationRecord
     after_save :update_post_comments_count
   
     def update_post_comments_count
-      post.increment!(:comments_conter)
+      post.increment!(:comments_counter)
     end
   end
